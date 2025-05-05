7318bb6f39d726539722a81be5ab11aa089a28f2
Dmitrii Okunev <xaionaro@dx.center>
2025-05-05 02:38:14 +0100

    Add UnsafePointer method to CVoid

--- types/void_ptr.go
+++ types/void_ptr.go
@@ -1,5 +1,14 @@
 package types
 
 import "C"
+import "unsafe"
 
-type CVoid = C.void
+type CVoid C.void
+
+func (ptr *CVoid) UnsafePointer() unsafe.Pointer {
+	return unsafe.Pointer(ptr)
+}
+
+func AsCVoid[T any](in *T) *CVoid {
+	return (*CVoid)((unsafe.Pointer)(in))
+}
